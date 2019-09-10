81c6d137d5285abeb16040fea685bba5d8db8859
Miranda J Ignacio <mirandajignacio@gmail.com>
2019-09-10 13:14:42 -0300

    Reach Router Support

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, {FunctionComponent} from 'react'
 import ReactDOM from 'react-dom'
+import { Router, Link, RouteComponentProps } from "@reach/router"
 
-const App = () => {
-  return <h1>Holis</h1>
-}
+
+const Home: FunctionComponent<RouteComponentProps> = () => <div>Home <br></br>
+  <Link to="/about">About</Link>
+</div>
+
+const About: FunctionComponent<RouteComponentProps> = () => <div>About <br></br>
+  <Link to="/">Home</Link>
+</div>
+
+const App:FunctionComponent = () => 
+  <Router>
+    <Home path="/" />
+    <About path="/about" />
+  </Router>
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
