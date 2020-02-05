62e3b1587aa2b6b3e93c6786d2df89f136fc1a7a
Miranda J. Ignacio <mirandajignacio@gmail.com>
2020-02-05 12:42:06 -0300

    feat: add storybook

---
+++ stories/Typography.stories.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import React, { ReactElement } from 'react';
+import Title1 from '../src/components/Title1';
+
+export default {
+  title: 'Typography',
+};
+
+export const Title_1 = (): ReactElement => <Title1>TItulo 1</Title1>;
+export const Title_2 = (): ReactElement => <Title1>TItulo 2</Title1>;
